56f8bbd4f484ad6b28e8c703499381ac9726c32c
Suhani Isarka <suhaniisarka2607@gmail.com>
2022-07-27 18:22:37 +0530

    27-july backup

---
+++ 27-july backup/backend/online-vehicle-portal-backend/Ecommerce-Backend-Demo/src/main/java/ecommerce/controllers/SellerController.java
@@ -0,0 +1,36 @@
+package ecommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ecommerce.entities.Seller;
+import ecommerce.models.LoginDTO;
+import ecommerce.models.Response;
+import ecommerce.services.SellerService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/sellers")
+public class SellerController {
+
+	@Autowired private SellerService sellerService;
+	
+	@GetMapping
+	public ResponseEntity<?> findAllCustomers() {
+		List<Seller> result = sellerService.allSellers();
+		return Response.success(result);
+	}
+	
+	
+}
